fb57de20710185943e689155545e808b81239e0a
nftchef <0n10ndevision@gmail.com>
2022-11-11 14:50:51 -0800

    update provenance util

--- utils/provenance.js
+++ utils/provenance.js
@@ -1,11 +1,12 @@
-const keccak256 = require("keccak256");
-const fs = require("fs");
-const chalk = require("chalk");
-const path = require("path");
+import  keccak256 from "keccak256"
+import  fs from "fs"
+import  chalk from "chalk"
+import path from "path"
+
 const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 
-const { buildDir } = require(path.join(basePath, "/src/config.js"));
+import { buildDir } from "../src/config.js";
 // Read files from the build folder defined in config.
 const metadata = JSON.parse(
   fs.readFileSync(path.join(buildDir, `/json/_metadata.json`), "utf-8")
